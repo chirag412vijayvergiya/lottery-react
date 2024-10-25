42ff0252bd76a300ef949bca411593eddfb1eddf
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2024-10-25 13:37:07 +0530

    connected web3 instance with the react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [manager, setManager] = useState("");
+
+  // Fetch manager info on component mount
+  useEffect(() => {
+    async function fetchManager() {
+      const manager = await lottery.methods.manager().call();
+      setManager(manager);
+    }
+    fetchManager();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lottery Contract</h1>
+        <p>This contract is managed by {manager}</p>
       </header>
     </div>
   );
---
+++ src/lottery.js
@@ -0,0 +1,63 @@
+// Purpose: To interact with the deployed contract on the Rinkeby network.
+// Create the instance of the contract using the address and ABI of the deployed contract.
+import web3 from "./web3";
+require("dotenv").config();
+const address = process.env.DEPLOY_ADDRESS;
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+// Create the instance of the contract using the address and ABI of the deployed contract.
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new web3.eth.Contract(abi, address);
